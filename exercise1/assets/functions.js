186aa597d56da6927591532290043cdf71886557
Claudia Bautista <anberlinsf@gmail.com>
2017-05-31 09:43:56 -0300

    Function with a ES6 promise

--- exercise1/assets/functions.js
+++ exercise1/assets/functions.js
@@ -4,9 +4,40 @@ window.onload = function(){
 	document.getElementById('hide').style.opacity = "1";}, 900)
 }
 
-
-function clickme(){
+/* function clickme(){
 
 	alert("You click me!");
 
+} */
+
+function clickjoke(){
+
+	var joke = new Promise(function(done, reject){
+
+		var ajax = new XMLHttpRequest();
+		ajax.open('POST', "http://api.icndb.com/jokes/random", true);
+
+		ajax.onreadystatechange = function(){
+			if((ajax.readyState == 4) && (ajax.status == 200)){
+				var resp = JSON.parse(ajax.responseText);
+				done(resp.value.joke)
+							
+			} else if ((ajax.status == 403) && (ajax.status == 404)){
+				reject('Error');
+
+			}
+		} 
+		ajax.send();
+	})
+
+	.then(function(resp){
+		document.getElementById('joke').innerHTML = resp
+	})
+
+	.catch(function(err){
+		document.getElementById('joke').innerHTML = err
+
+	})
+
 }
+
