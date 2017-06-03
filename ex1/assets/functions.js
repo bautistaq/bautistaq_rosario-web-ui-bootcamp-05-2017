2b126dd1f408c78b0d69d0099a4dbe463e8156c5
Claudia Bautista <anberlinsf@gmail.com>
2017-06-03 19:41:43 -0300

    Class movie and actor created

---
+++ ex1/assets/functions.js
@@ -0,0 +1,23 @@
+class movie{
+
+	constructor(title, year, duration){
+	
+		this.title = title;
+		this.year = year;
+		this.duration = duration;
+	}
+
+}
+
+class actor{
+	constructor(name, age){
+		this.name = name;
+		this.age = age;
+	}
+
+
+}
+
+var mipeli = new movie('Buscando a Nemo','2005','100 min'); 
+
+var miactor = new actor('yo', '123')
